a2b6dab3a92863cce681d926face37a78575e939
Viktor Gurev <siplyj2006@yandex.ru>
2019-10-20 23:42:44 +0300

    The first version of the project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Pinterest-downloader
+With this extension, you will open the current image from Pinterest in a new browser tab.
+And it can find a picture in Google, Yandex and Bing.
---
+++ popup.js
@@ -0,0 +1,61 @@
+function click(e) {
+
+	if (e.target.id == 'openImage') { // Open in new tab
+
+		chrome.tabs.executeScript(null, {
+			code: `
+			if (document.location.href.startsWith('https://www.pinterest.com/pin/')) {
+				window.open(document.getElementsByClassName("hCL")[2].src, "_blank")
+			}`
+		});
+
+	} else if (e.target.id == 'searchByGoogle') { // Search by Google
+
+		chrome.tabs.executeScript(null, {
+			code: `
+			if (document.location.href.startsWith('https://www.pinterest.com/pin/')) {
+				let imgUrl = document.getElementsByClassName("hCL")[2].src;
+				window.open('https://www.google.com/searchbyimage?image_url=' + imgUrl);
+			} else if (document.location.href.startsWith('https://i.pinimg.com/')) {
+				let imgUrl = document.location.href;
+				window.open('https://www.google.com/searchbyimage?image_url=' + imgUrl);
+			}`
+		});
+
+	} else if (e.target.id == 'searchByYandex') { // Search by Yandex
+
+		chrome.tabs.executeScript(null, {
+			code: `
+			if (document.location.href.startsWith('https://www.pinterest.com/pin/')) {
+				let imgUrl = document.getElementsByClassName("hCL")[2].src;
+				window.open('https://yandex.ru/images/search?url=' + imgUrl + '&rpt=imageview');
+			} else if (document.location.href.startsWith('https://i.pinimg.com/')) {
+				let imgUrl = document.location.href;
+				window.open('https://yandex.ru/images/search?url=' + imgUrl + '&rpt=imageview');
+			}`
+		});
+
+	} else if (e.target.id == 'searchByBing') { // Search by Bing
+
+		chrome.tabs.executeScript(null, {
+			code: `
+			if (document.location.href.startsWith('https://www.pinterest.com/pin/')) {
+				let imgUrl = document.getElementsByClassName("hCL")[2].src;
+				window.open('https://www.bing.com/images/search?view=detailv2&iss=sbi&q=imgurl:' + imgUrl);
+			} else if (document.location.href.startsWith('https://i.pinimg.com/')) {
+				let imgUrl = document.location.href;
+				window.open('https://www.bing.com/images/search?view=detailv2&iss=sbi&q=imgurl:' + imgUrl);
+			}`
+		});
+	} 
+
+	window.close();
+	
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	let buttons = document.querySelectorAll('button');
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', click);
+	}
+});
